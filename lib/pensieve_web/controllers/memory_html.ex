62e8e4eac2e32754675fcbca7804aa83e16d1b90
kimihito <tatsurotamashiro@gmail.com>
2024-03-19 22:51:03 +0900

    extract <.information> component

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -2,4 +2,13 @@ defmodule PensieveWeb.MemoryHTML do
   use PensieveWeb, :html
 
   embed_templates "/memory_html/*"
+
+  attr :memories, :list, default: []
+  def information(assigns) do
+    ~H"""
+    <div class="italic mb-4">
+      These are <%= length(@memories) %> memories in your Pensieve
+    </div>
+    """
+  end
 end
